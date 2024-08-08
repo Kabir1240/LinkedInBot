144a06c0c10fbb8804468c2624372475b90771cb
Kabir Kashif <kashif.kabir60@gmail.com>
2024-08-08 19:42:17 +0500

    updated gitignore, added function for login

--- main.py
+++ main.py
@@ -2,5 +2,24 @@ import time
 import os
 from selenium import  webdriver
 from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+user_data_dir = os.environ.get("USER_DATA_DIR")
+url = "https://www.linkedin.com/"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+# chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+chrome_options.add_argument("--incognito")
 
 
+def login():
+    pass
+
+driver = webdriver.Chrome(options=chrome_options)
+
+# goto the cookie clicker site
+driver.get(url)
+login()
